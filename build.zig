99642030466464c6314aed2b571b5b5eb5a339fe
wanqiang <wanqiang@shizhuang-inc.com>
2024-12-17 10:23:56 +0800

    feat: Added skiplist unit tests - refactor: Refactored SkipList deinit and display functions;test: Added test for SkipList functionality

--- build.zig
+++ build.zig
@@ -80,6 +80,13 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const skiplist_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/skiplist.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_skiplist_unit_tests = b.addRunArtifact(skiplist_unit_tests);
+
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
@@ -88,4 +95,5 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_skiplist_unit_tests.step);
 }
