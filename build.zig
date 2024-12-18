003dec587251d812775bbdb11ad866d70e026b68
wanqiang <wanqiang@shizhuang-inc.com>
2024-12-18 18:35:46 +0800

    fix: Update .gitignore to include /tmp/ directory feat: Add MemTable unit tests chore: Update build.zig to include MemTable unit tests and remove WAL unit tests

--- build.zig
+++ build.zig
@@ -85,26 +85,19 @@ pub fn build(b: *std.Build) void {
     });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    const skiplist_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/skiplist.zig"),
+    const mem_table_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/MemTable.zig"),
         .target = target,
         .optimize = optimize,
     });
-    const run_skiplist_unit_tests = b.addRunArtifact(skiplist_unit_tests);
-
-    const wal_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/WAL.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    wal_unit_tests.linkLibC();
-    wal_unit_tests.addIncludePath(LazyPath{ .cwd_relative = "./include" });
-    wal_unit_tests.addCSourceFiles(.{
+    mem_table_unit_tests.linkLibC();
+    mem_table_unit_tests.addIncludePath(LazyPath{ .cwd_relative = "./include" });
+    mem_table_unit_tests.addCSourceFiles(.{
         .files = &c_source_files,
         .flags = &c_flags,
     });
 
-    const run_wal_unit_tests = b.addRunArtifact(wal_unit_tests);
+    const run_mem_table_unit_tests = b.addRunArtifact(mem_table_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
@@ -112,6 +105,5 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
-    test_step.dependOn(&run_skiplist_unit_tests.step);
-    test_step.dependOn(&run_wal_unit_tests.step);
+    test_step.dependOn(&run_mem_table_unit_tests.step);
 }
