be919b1bc65f1d92ba381e1ad231491f35e4283f
wanqiang <wanqiang@shizhuang-inc.com>
2024-12-23 16:48:34 +0800

    fix: Update test configuration to use storage unit tests instead of mem_table unit tests.

--- build.zig
+++ build.zig
@@ -85,19 +85,19 @@ pub fn build(b: *std.Build) void {
     });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    const mem_table_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/MemTable.zig"),
+    const storage_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/storage.zig"),
         .target = target,
         .optimize = optimize,
     });
-    mem_table_unit_tests.linkLibC();
-    mem_table_unit_tests.addIncludePath(LazyPath{ .cwd_relative = "./include" });
-    mem_table_unit_tests.addCSourceFiles(.{
+    storage_unit_tests.linkLibC();
+    storage_unit_tests.addIncludePath(LazyPath{ .cwd_relative = "./include" });
+    storage_unit_tests.addCSourceFiles(.{
         .files = &c_source_files,
         .flags = &c_flags,
     });
 
-    const run_mem_table_unit_tests = b.addRunArtifact(mem_table_unit_tests);
+    const run_storage_unit_tests = b.addRunArtifact(storage_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
@@ -105,5 +105,5 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
-    test_step.dependOn(&run_mem_table_unit_tests.step);
+    test_step.dependOn(&run_storage_unit_tests.step);
 }
