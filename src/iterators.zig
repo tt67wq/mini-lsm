6f027316d9d3c421cbf874f549c541e11732b28e
wanqiang <wanqiang@shizhuang-inc.com>
2024-12-26 20:00:59 +0800

    feat: Add LsmIterator to StorageIterator fix: Update isEmpty and next methods for LsmIterator chore: Add LsmIterator and related imports chore: Refactor LsmIterator to use MergeIterators chore: Add Bound struct and related methods to LsmIterator

--- src/iterators.zig
+++ src/iterators.zig
@@ -1,13 +1,19 @@
 const std = @import("std");
 const MemTable = @import("MemTable.zig");
 const MemTableIterator = MemTable.MemTableIterator;
+const lsm_iterator = @import("lsm_iterators.zig");
+const LsmIterator = lsm_iterator.LsmIterator;
 
 pub const StorageIterator = union(enum) {
     mem_iter: MemTableIterator,
+    lsm_iter: LsmIterator,
 
     pub fn isEmpty(self: StorageIterator) bool {
         switch (self) {
             .mem_iter => |iter| {
+                return iter.isEmpty();
+            },
+            .lsm_iter => |iter| {
                 return iter.isEmpty();
             },
         }
@@ -16,6 +22,7 @@ pub const StorageIterator = union(enum) {
     pub fn next(self: *StorageIterator) void {
         switch (self.*) {
             .mem_iter => self.mem_iter.next(),
+            .lsm_iter => self.lsm_iter.next(),
         }
     }
 
@@ -24,12 +31,18 @@ pub const StorageIterator = union(enum) {
             .mem_iter => |iter| {
                 return iter.key();
             },
+            .lsm_iter => |iter| {
+                return iter.key();
+            },
         }
     }
 
     pub fn value(self: StorageIterator) ?[]const u8 {
         switch (self) {
             .mem_iter => |iter| {
+                return iter.value();
+            },
+            .lsm_iter => |iter| {
                 return iter.value();
             },
         }
