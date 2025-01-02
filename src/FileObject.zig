cb897206f8e6c4f9b2e7c659b01b8945eda47c68
wanqiang <wanqiang@shizhuang-inc.com>
2025-01-02 13:59:10 +0800

    fix: Corrected return statement formatting in FileObject.zig feat: Added test cases for FileObject functionality

--- src/FileObject.zig
+++ src/FileObject.zig
@@ -15,7 +15,7 @@ pub fn init(path: []const u8, data: []const u8) !Self {
     var f = try fs.cwd().openFile(path, .{ .mode = .read_only });
     try f.sync();
 
-    return .Self{
+    return .{
         .file = f,
         .size = data.len,
     };
@@ -24,7 +24,7 @@ pub fn init(path: []const u8, data: []const u8) !Self {
 pub fn open(path: []const u8) !Self {
     var f = try fs.cwd().openFile(path, .{ .mode = .read_only });
     const md = try f.metadata();
-    return .Self{
+    return .{
         .file = f,
         .size = md.size(),
     };
@@ -36,4 +36,22 @@ pub fn deinit(self: Self) void {
 
 pub fn read(self: Self, offset: u64, buf: []u8) !usize {
     return try self.file.pread(buf, offset);
+}
+
+test "file" {
+    var f = try Self.init("./tmp/test.txt", "hello world");
+    defer f.deinit();
+
+    var buf: [11]u8 = undefined;
+    const sz = try f.read(0, buf[0..]);
+    try std.testing.expectEqual(11, sz);
+    try std.testing.expectEqualSlices(u8, "hello world", buf[0..]);
+
+    var f2 = try Self.open("./tmp/test.txt");
+    defer f2.deinit();
+
+    var buf2: [5]u8 = undefined;
+    const sz2 = try f2.read(6, buf2[0..]);
+    try std.testing.expectEqual(5, sz2);
+    try std.testing.expectEqualSlices(u8, "world", buf2[0..]);
 }
