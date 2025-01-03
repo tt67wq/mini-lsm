865467fe42e48982ee18ae1bcf6f4db9f95de752
tt67wq <tt67wq@aliyun.com>
2025-01-03 12:32:32 +0800

    feat: Add BitArray and BloomFilter implementations fix: Correct calculation of m and k in BloomFilter test: Add tests for BitArray and BloomFilter

---
+++ src/bloom_filter/BitArray.zig
@@ -0,0 +1,120 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const Allocator = std.mem.Allocator;
+
+const BitArrayError = error{
+    UnsupportedArraySize,
+    InvalidBitOffset,
+};
+
+allocator: Allocator,
+/// Slice of bytes used as bit store.
+bytes: []u8,
+/// Length in bits of the bit array.
+len: u64,
+
+const Self = @This();
+
+/// Initialize new BitArray given allocator and length in bits of BitArray.
+pub fn init(allocator: Allocator, num_bits: u64) !Self {
+    // determine number of bytes of memory needed
+    const num_bytes: u64 = if (num_bits % 8 > 0) (num_bits / 8) + 1 else (num_bits / 8);
+
+    // check requested bit array size is supported by machine arch
+    if (num_bytes > std.math.maxInt(usize)) return BitArrayError.UnsupportedArraySize;
+    const bytes = try allocator.alloc(u8, num_bytes);
+    @memset(bytes, 0);
+
+    return .{
+        .allocator = allocator,
+        .bytes = bytes,
+        .len = num_bits,
+    };
+}
+
+/// Deinitialize and free resources
+pub fn deinit(self: *Self) void {
+    self.allocator.free(self.bytes);
+}
+
+/// Get value of bit.
+pub fn getBit(self: Self, idx: u64) BitArrayError!u1 {
+    try self.isValidBitIdx(idx);
+    const offset = bitOffset(idx);
+    return @truncate(
+        ((self.bytes[byteIdx(idx)] & (@as(u8, 1) << offset))) >> offset,
+    );
+}
+
+/// Set value of bit to 1.
+pub fn setBit(self: *Self, idx: u64) BitArrayError!void {
+    try self.isValidBitIdx(idx);
+    self.bytes[byteIdx(idx)] |= @as(u8, 1) << bitOffset(idx);
+}
+
+/// Clear value of bit to 0.
+pub fn clearBit(self: *Self, idx: u64) BitArrayError!void {
+    try self.isValidBitIdx(idx);
+    self.bytes[byteIdx(idx)] &= ~(@as(u8, 1) << bitOffset(idx));
+}
+
+/// Toggle value of bit.
+pub fn toggleBit(self: *Self, idx: u64) BitArrayError!void {
+    try self.isValidBitIdx(idx);
+    self.bytes[byteIdx(idx)] ^= @as(u8, 1) << bitOffset(idx);
+}
+
+fn isValidBitIdx(self: Self, idx: u64) BitArrayError!void {
+    if (idx >= self.len) return BitArrayError.InvalidBitOffset;
+}
+
+fn byteIdx(bit_idx: u64) usize {
+    const byte_idx: u64 = bit_idx / 8;
+
+    // @truncate of byte_idx from u64 to usize will never truncate any
+    // significant as we always check that bit_idx < the length of the bit
+    // array, whose size in bytes is constrained at initialization to be always
+    // addressable by a usize pointer.
+    return @truncate(byte_idx);
+}
+
+fn bitOffset(bit_idx: u64) u3 {
+    return @truncate(bit_idx % 8);
+}
+
+test "init_deinit" {
+    var bits = try Self.init(std.testing.allocator, 1000);
+    defer bits.deinit();
+}
+
+test "get" {
+    var bits = try Self.init(std.testing.allocator, 1000);
+    defer bits.deinit();
+
+    try expect(0 == try bits.getBit(200));
+}
+
+test "set" {
+    var bits = try Self.init(std.testing.allocator, 1000);
+    defer bits.deinit();
+
+    try bits.setBit(200);
+    try expect(1 == try bits.getBit(200));
+}
+
+test "clear" {
+    var bits = try Self.init(std.testing.allocator, 1000);
+    defer bits.deinit();
+
+    try bits.setBit(400);
+    try bits.clearBit(400);
+    try expect(0 == try bits.getBit(400));
+}
+
+test "toggle" {
+    var bits = try Self.init(std.testing.allocator, 1000);
+    defer bits.deinit();
+
+    try bits.toggleBit(400);
+    try expect(1 == try bits.getBit(400));
+}
---
+++ src/bloom_filter/BloomFilter.zig
@@ -0,0 +1,127 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const math = std.math;
+const Allocator = std.mem.Allocator;
+const BitArray = @import("BitArray.zig");
+const Murmur = std.hash.Murmur3_32;
+
+const BloomFilterError = error{
+    UnsupportedSpec,
+};
+
+/// Bloom filter with allocation of bit array at runtime initialization. Uses
+/// Murmur3 hash functions.
+num_hash_funcs: u8,
+bits: BitArray,
+
+const Self = @This();
+
+/// Initialize a new Bloom filter that is configured and sized to hold
+/// a specified maximum number of items with a given false positive rate.
+pub fn init(
+    allocator: std.mem.Allocator,
+    max_items: u64,
+    fp_rate: f32,
+) !Self {
+    const m = calcM(max_items, fp_rate);
+    const k = calcK(max_items, m);
+
+    // check `k` is < 256
+    if (k > std.math.maxInt(u8)) {
+        return BloomFilterError.UnsupportedSpec;
+    }
+
+    return .{
+        .num_hash_funcs = @truncate(k),
+        .bits = try BitArray.init(allocator, m),
+    };
+}
+
+/// Deinitialize and free resources
+pub fn deinit(self: *Self) void {
+    self.bits.deinit();
+}
+
+/// Insert an item into the Bloom filter.
+pub fn insert(self: *Self, item: []const u8) !void {
+    for (0..self.num_hash_funcs) |i| {
+        try self.bits.setBit(self.calcBitIdx(item, @truncate(i)));
+    }
+}
+
+/// Returns whether Bloom filter contains the item. It may return a false
+/// positive, but will never return a false negative.
+pub fn contains(self: Self, item: []const u8) !bool {
+    for (0..self.num_hash_funcs) |i| {
+        if (try self.bits.getBit(self.calcBitIdx(item, @truncate(i))) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/// Calculate index of bit for given item and hashing function seed
+fn calcBitIdx(self: Self, item: []const u8, hash_seed: u32) u64 {
+    const hash = Murmur.hashWithSeed(item, hash_seed);
+    return hash % self.bits.len;
+}
+
+/// Calculate the appropriate number in bits of the Bloom filter, `m`, given
+/// `n`, the expected number of elements contained in the Bloom filter and the
+/// target false positive rate, `f`.
+///
+/// `(-nln(f))/ln(2)^2`
+fn calcM(n: u64, f: f64) u64 {
+    const numerator = @as(f64, @floatFromInt(n)) * -math.log(f64, math.e, f);
+    const denominator = math.pow(f64, (math.log(f64, math.e, 2)), 2);
+    return @intFromFloat(
+        math.divTrunc(f64, numerator, denominator) catch unreachable,
+    );
+}
+
+/// Calculate the number of hash functions to use, `k`, given `n` and `m`, the expected
+/// number of elements contained in the Bloom filter and the size in bits of the Bloom
+/// filter.
+///
+/// `(mln(2)/n)`
+fn calcK(n: u64, m: u64) u64 {
+    // https://en.wikipedia.org/wiki/Bloom_filter#Optimal_number_of_hash_functions
+    const numerator = @as(f64, @floatFromInt(m)) * math.log(f64, math.e, 2);
+    const denominator = @as(f64, @floatFromInt(n));
+    return @as(u64, @intFromFloat(
+        math.divTrunc(f64, numerator, denominator) catch unreachable,
+    ));
+}
+
+test "calcM" {
+    const n: usize = 1_000_000;
+    const f: f32 = 0.02;
+    try expect(calcM(n, f) == 8_142_363);
+}
+
+test "calcK" {
+    const n: usize = 1_000_000;
+    const m: usize = 8_142_363;
+    try expect(calcK(n, m) == 5);
+}
+
+test "init" {
+    var filter = try Self.init(std.testing.allocator, 100_000, 0.02);
+    defer filter.deinit();
+}
+
+test "contains_true" {
+    var filter = try Self.init(std.testing.allocator, 100_000, 0.02);
+    defer filter.deinit();
+
+    try filter.insert("hi");
+    try expect(try filter.contains("hi") == true);
+}
+
+test "contains_false" {
+    var filter = try Self.init(std.testing.allocator, 100_000, 0.02);
+    defer filter.deinit();
+
+    try filter.insert("hi");
+    try expect(try filter.contains("yo") == false);
+}
