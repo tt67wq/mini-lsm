5ff4d3dd409469e997c649de6c4b5d665bc6e050
tt67wq <tt67wq@aliyun.com>
2025-01-15 12:10:08 +0800

    feat: Add numActiveIterators function to MergeIterators fix: Update StorageIterator to handle new numActiveIterators method chore: Remove lsm_iterators.zig as it's no longer needed

--- src/iterators.zig
+++ src/iterators.zig
@@ -1,63 +1,176 @@
 const std = @import("std");
 const MemTable = @import("MemTable.zig");
-const MemTableIterator = MemTable.MemTableIterator;
-const lsm_iterator = @import("lsm_iterators.zig");
-const LsmIterator = lsm_iterator.LsmIterator;
 const ss_table = @import("ss_table.zig");
+const MergeIterators = @import("MergeIterators.zig");
+const Bound = @import("MemTable.zig").Bound;
+const MemTableIterator = MemTable.MemTableIterator;
 const SsTableIterator = ss_table.SsTableIterator;
 
 pub const StorageIterator = union(enum) {
     mem_iter: MemTableIterator,
     lsm_iter: LsmIterator,
     ss_table_iter: SsTableIterator,
+    merge_iterators: MergeIterators,
+    two_merge_iterators: TwoMergeIterator,
 
     pub fn isEmpty(self: StorageIterator) bool {
         switch (self) {
-            .mem_iter => |iter| {
-                return iter.isEmpty();
-            },
-            .lsm_iter => |iter| {
-                return iter.isEmpty();
-            },
-            .ss_table_iter => |iter| {
-                return iter.isEmpty();
-            },
+            inline else => |impl| return impl.isEmpty(),
         }
     }
 
     pub fn next(self: *StorageIterator) void {
         switch (self.*) {
-            .mem_iter => self.mem_iter.next(),
-            .lsm_iter => self.lsm_iter.next(),
-            .ss_table_iter => self.ss_table_iter.next(),
+            inline else => |impl| impl.next(),
         }
     }
 
     pub fn key(self: StorageIterator) []const u8 {
         switch (self) {
-            .mem_iter => |iter| {
-                return iter.key();
-            },
-            .lsm_iter => |iter| {
-                return iter.key();
-            },
-            .ss_table_iter => |iter| {
-                return iter.key();
-            },
+            inline else => |impl| return impl.key(),
         }
     }
 
     pub fn value(self: StorageIterator) ?[]const u8 {
         switch (self) {
-            .mem_iter => |iter| {
-                return iter.value();
+            inline else => |impl| return impl.value(),
+        }
+    }
+
+    pub fn numActiveIterators(self: StorageIterator) usize {
+        switch (self) {
+            .merge_iterators => |iter| return iter.numActiveIterators(),
+            .lsm_iter => |iter| return iter.numActiveIterators(),
+            inline else => return 1,
+        }
+    }
+};
+
+const LsmIteratorInner = MergeIterators;
+
+pub const LsmIterator = struct {
+    inner: LsmIteratorInner,
+    end_bound: Bound,
+    is_empty: bool,
+
+    pub fn init(
+        inner: LsmIteratorInner,
+        end_bound: Bound,
+    ) LsmIterator {
+        return LsmIterator{
+            .inner = inner,
+            .end_bound = end_bound,
+            .is_empty = inner.isEmpty(),
+        };
+    }
+
+    pub fn deinit(self: *LsmIterator) void {
+        self.inner.deinit();
+    }
+
+    pub fn isEmpty(self: LsmIterator) bool {
+        return self.is_empty;
+    }
+
+    pub fn key(self: LsmIterator) []const u8 {
+        return self.inner.key();
+    }
+
+    pub fn value(self: LsmIterator) ?[]const u8 {
+        return self.inner.value();
+    }
+
+    pub fn next(self: *LsmIterator) void {
+        self.inner.next();
+        if (self.inner.isEmpty()) {
+            self.is_empty = true;
+            return;
+        }
+        switch (self.end_bound.bound_t) {
+            .unbounded => return,
+            .included => {
+                self.is_empty = std.mem.lessThan(u8, self.key(), self.end_bound.data) or
+                    std.mem.eql(u8, self.key(), self.end_bound.data);
+                return;
             },
-            .lsm_iter => |iter| {
-                return iter.value();
-            },
-            .ss_table_iter => |iter| {
-                return iter.value();
+            .excluded => {
+                self.is_empty = std.mem.lessThan(u8, self.key(), self.end_bound.data);
+                return;
             },
         }
+    }
+
+    pub fn numActiveIterators(self: LsmIterator) usize {
+        return self.inner.numActiveIterators();
+    }
+};
+
+pub const TwoMergeIterator = struct {
+    a: StorageIterator,
+    b: StorageIterator,
+    choose_a: bool,
+
+    fn chooseA(a: StorageIterator, b: StorageIterator) bool {
+        if (a.isEmpty()) {
+            return false;
+        }
+        if (b.isEmpty()) {
+            return true;
+        }
+        return std.mem.lessThan(u8, a.key(), b.key());
+    }
+
+    fn skipB(self: *TwoMergeIterator) void {
+        if (!self.a.isEmpty() and !self.b.isEmpty() and std.mem.eql(u8, self.a.key(), self.b.key())) self.b.next();
+    }
+
+    fn init(a: StorageIterator, b: StorageIterator) TwoMergeIterator {
+        var iter = TwoMergeIterator{
+            .a = a,
+            .b = b,
+            .choose_a = false,
+        };
+        iter.skipB();
+        iter.choose_a = chooseA(a, b);
+        return iter;
+    }
+
+    pub fn key(self: TwoMergeIterator) []const u8 {
+        if (self.choose_a) {
+            std.debug.assert(!self.a.isEmpty());
+            return self.a.key();
+        }
+        std.debug.assert(!self.b.isEmpty());
+        return self.b.key();
+    }
+
+    pub fn value(self: TwoMergeIterator) ?[]const u8 {
+        if (self.choose_a) {
+            std.debug.assert(!self.a.isEmpty());
+            return self.a.value();
+        }
+        std.debug.assert(!self.b.isEmpty());
+        return self.b.value();
+    }
+
+    pub fn isEmpty(self: TwoMergeIterator) bool {
+        if (self.choose_a) {
+            return self.a.isEmpty();
+        }
+        return self.b.isEmpty();
+    }
+
+    pub fn next(self: *TwoMergeIterator) void {
+        if (self.choose_a) {
+            self.a.next();
+        } else {
+            self.b.next();
+        }
+        self.skipB();
+        self.choose_a = chooseA(self.a, self.b);
+    }
+
+    pub fn numActiveIterators(self: TwoMergeIterator) usize {
+        return self.a.numActiveIterators() + self.b.numActiveIterators();
     }
 };
