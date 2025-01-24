36ce39fb56a0d03405c213e9f30de151e01488dd
tt67wq <tt67wq@aliyun.com>
2025-01-24 16:43:33 +0800

    fix: Remove unused Stopper import and use ResetEvent for thread termination fix: Refactor syncDir to use a recursive approach fix: Remove unused Stopper from StorageInner fix: Use WaitGroup to manage compaction thread feat: Add syncDir function to StorageInner feat: Refactor flushLoop to use ResetEvent for thread termination

--- src/MiniLsm.zig
+++ src/MiniLsm.zig
@@ -5,24 +5,22 @@ const Self = @This();
 
 allocator: std.mem.Allocator,
 inner: storage.StorageInner,
-compaction_stopper: storage.Stopper,
 
 pub fn init(allocator: std.mem.Allocator, path: []const u8, options: storage.StorageOptions) !Self {
     var inner = try storage.StorageInner.init(allocator, path, options);
     errdefer inner.deinit();
 
-    var stopper = try inner.spawnCompactionThread();
-    errdefer stopper.deinit();
+    inner.spawnCompactionThread();
 
     return .{
         .allocator = allocator,
         .inner = inner,
-        .compaction_stopper = stopper,
     };
 }
 
 pub fn deinit(self: *Self) void {
-    self.compaction_stopper.send(struct {}{});
-    self.compaction_stopper.deinit();
+    self.inner.syncDir() catch |err| {
+        std.log.err("failed to sync dir: {s}", .{@errorName(err)});
+    };
     self.inner.deinit();
 }
