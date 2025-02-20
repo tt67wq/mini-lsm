74ed08feb33e6ac3acd8f8c6cf99f2ff88d8a671
wanqiang <wanqiang@shizhuang-inc.com>
2025-02-20 15:18:29 +0800

    refactor: Update MiniLsm.zig to use StorageInnerPtr for inner storage management.

--- src/MiniLsm.zig
+++ src/MiniLsm.zig
@@ -9,66 +9,66 @@ pub const Bound = @import("MemTable.zig").Bound;
 const Self = @This();
 
 allocator: std.mem.Allocator,
-inner: storage.StorageInner,
+inner: storage.StorageInnerPtr,
 
 pub fn init(allocator: std.mem.Allocator, path: []const u8, options: StorageOptions) !Self {
     var inner = try storage.StorageInner.init(allocator, path, options);
     errdefer inner.deinit();
 
-    try inner.syncDir();
+    const ptr = try storage.StorageInnerPtr.create(allocator, inner);
 
-    inner.spawnFlushThread();
-    inner.spawnCompactionThread();
+    ptr.get().spawnFlushThread();
+    ptr.get().spawnCompactionThread();
 
     return .{
         .allocator = allocator,
-        .inner = inner,
+        .inner = ptr,
     };
 }
 
 pub fn deinit(self: *Self) !void {
-    try self.inner.syncDir();
-    self.inner.deinit();
+    try self.inner.get().syncDir();
+    self.inner.get().deinit();
 }
 
 pub fn get(self: *Self, key: []const u8, val: *[]const u8) !bool {
-    return self.inner.get(key, val);
+    return self.inner.get().get(key, val);
 }
 
 pub fn put(self: *Self, key: []const u8, val: []const u8) !void {
-    return self.inner.put(key, val);
+    return self.inner.get().put(key, val);
 }
 
 pub fn del(self: *Self, key: []const u8) !void {
-    return self.inner.delete(key);
+    return self.inner.get().delete(key);
 }
 
 pub fn writeBatch(self: *Self, batch: []const WriteBatchRecord) !void {
-    return self.inner.writeBatch(batch);
+    return self.inner.get().writeBatch(batch);
 }
 
 pub fn sync(self: *Self) !void {
-    return self.inner.sync();
+    return self.inner.get().sync();
 }
 
 pub fn scan(self: *Self, lower: Bound, upper: Bound) !LsmIterator {
-    return self.inner.scan(lower, upper);
+    return self.inner.get().scan(lower, upper);
 }
 
 pub fn forceFlush(self: *Self) !void {
     var is_empty: bool = false;
     {
-        self.inner.state_lock.lockShared();
-        defer self.inner.state_lock.unlockShared();
-        is_empty = self.inner.state.getMemTable().isEmpty();
+        self.inner.get().state_lock.lockShared();
+        defer self.inner.get().state_lock.unlockShared();
+        is_empty = self.inner.get().state.getMemTable().isEmpty();
     }
 
     if (!is_empty) {
-        try self.inner.forceFreezeMemtable();
-        try self.inner.flushNextMemtable();
+        try self.inner.get().forceFreezeMemtable();
+        try self.inner.get().flushNextMemtable();
     }
 }
 
 pub fn forceFullCompaction(self: *Self) !void {
-    try self.inner.forceFullCompaction();
+    try self.inner.get().forceFullCompaction();
 }
